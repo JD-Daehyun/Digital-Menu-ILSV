9227d1906d338cd0fcaa3f488a2765671baee3aa
Daehyun Jung <daehyunj1997@gmail.com>
2024-05-20 12:13:50 -0700

    Add console to debugg inputform js

--- JS/input_form_script.js
+++ JS/input_form_script.js
@@ -1,6 +1,8 @@
 document.getElementById('menuForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
+    console.log('Form submitted');
+
     const name = document.getElementById('name').value;
     const description = document.getElementById('description').value;
     const price = document.getElementById('price').value;
@@ -16,15 +18,19 @@ document.getElementById('menuForm').addEventListener('submit', function(event) {
             highlight: highlight,
             image: reader.result
         };
+    
+        console.log('Menu item:', menuItem);
 
         let menuItems = JSON.parse(localStorage.getItem('menuItems')) || [];
         menuItems.push(menuItem);
         localStorage.setItem('menuItems', JSON.stringify(menuItems));
 
-        window.location.href = 'index.html';
+        // window.location.href = 'index.html';
     };
 
     if (image) {
         reader.readAsDataURL(image);
+    } else {
+        console.error('No image file selected');
     }
 });
